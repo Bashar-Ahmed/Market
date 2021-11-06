cce1f107222f7f8236bcedf369bf5e982c4fd197
Bashar-Ahmed <basharahmed00000@gmail.com>
2021-11-06 12:55:25 +0530

    conflict resolved

---
+++ app/controllers/admin/updatequant.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controller;
+
+class Updatequant{
+
+    public function post() {
+        \Controller\Util::check_session_ifnotset("/","admin");
+        \Controller\Util::check_post("/","iditem");
+        $newquant = $_POST["newquant"];
+        \Model\Item::quantupdate($_POST["iditem"],$newquant);
+        header("Location: /itemlist-admin");
+    }
+
+}
